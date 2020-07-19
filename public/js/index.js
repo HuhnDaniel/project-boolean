48aca96ffc4970c4134d4ffb98510bcaa31b9846
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-07-19 15:36:31 -0500

    lodash update (security fix), remove duplicate/example code

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,5 @@
 // Get references to page elements
 const $ = window.$;
-const $exampleText = $('#example-text');
-const $exampleDescription = $('#example-description');
-const $submitBtn = $('#submit');
-const $exampleList = $('#example-list');
 const $deleteBtn = $('.deleteBtn');
 
 // The API object contains methods for each kind of request we'll make
